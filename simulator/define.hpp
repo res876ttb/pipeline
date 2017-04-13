f79041a2ec4f7f81ba1fba1faa39c7dad83c4978
res876ttb <someone10851222@gmail.com>
2017-04-13 18:10:12 +0800

    update

--- simulator/define.hpp
+++ simulator/define.hpp
@@ -2,8 +2,15 @@
 #define log
 
 // variable definition
-
+int regi[32];
+int memi[256];
+char memd[1024];
+int regi2[32];
+int PC;
+int IF_PC;
+int IF_Instruction_code;
+int ID_Register_Result[2];
 
 // functions definition
-void init();
-void read_data();
+inline void init();
+inline void read_data();
--- simulator/pipeline.cpp
+++ simulator/pipeline.cpp
@@ -8,11 +8,25 @@ int main() {
 }
 
 void init() {
+#ifdef log
+	printf(">>> init()\n");	
+#endif
+	
 	read_data();
+	
+#ifdef log
+	printf(">>> init() done\n");
+#endif
 }
 
 void read_data() {
+#ifdef log
+	printf(">>> read_data()\n");
+#endif
+	
 	char buff[4];
+	int sizeofchar = sizeof(char);
+	int tmp, data_size;
 	FILE *iimage, *dimage;
 	iimage = fopen("iimage.bin", "rb");
 	dimage = fopen("dimage.bin", "rb");
@@ -24,16 +38,35 @@ void read_data() {
 		printf("No iimage.bin\n");
 	}
 	
+#ifdef log
+	printf(">>> Reading dimage.bin...  ");
+#endif
 	// read dimage.bin
-	int sizeofchar = sizeof(char);
-	int tmp;
-	tmp = fread(buff, sizeofchar, 4, dimage);
-	printf("buf = %X%X%X%X%X%X%X%X\n", buff[0]/16, buff[0]%16, buff[1]/16, buff[1]%16, buff[2]/16, buff[2]%16, buff[3]/16, buff[3]%16);
-	printf("tmp = %d\n", tmp);
-	tmp = fread(buff, sizeofchar, 4, dimage);
-	printf("buf = %X%X%X%X%X%X%X%X\n", buff[0]/16, buff[0]%16, buff[1]/16, buff[1]%16, buff[2]/16, buff[2]%16, buff[3]/16, buff[3]%16);
-	printf("tmp = %d\n", tmp);
-	tmp = fread(buff, sizeofchar, 4, dimage);
-	printf("buf = %X%X%X%X%X%X%X%X\n", buff[0]/16, buff[0]%16, buff[1]/16, buff[1]%16, buff[2]/16, buff[2]%16, buff[3]/16, buff[3]%16);
-	printf("tmp = %d\n", tmp);
+	tmp = fread(buff, sizeofchar, 4, dimage); // read initial SP address
+	regi[29] = regi2[29] =  (((unsigned char)buff[0])<<24)|(((unsigned char)buff[1])<<16)|(((unsigned char)buff[2])<<8)|(((unsigned char)buff[3]));
+	tmp = fread(buff, sizeofchar, 4, dimage); // read total number of data
+	data_size = (((unsigned char)buff[0])<<24)|(((unsigned char)buff[1])<<16)|(((unsigned char)buff[2])<<8)|(((unsigned char)buff[3]));
+	tmp = fread(memd, sizeofchar, data_size * 4, dimage);
+#ifdef log
+	printf("done\n");
+#endif
+	
+#ifdef log
+	printf(">>> Reading iimage.bin...  ");
+#endif
+	// read iimage.bin
+	tmp = fread(buff, sizeofchar, 4, iimage); // read initial PC address
+	PC = (((unsigned char)buff[0])<<24)|(((unsigned char)buff[1])<<16)|(((unsigned char)buff[2])<<8)|(((unsigned char)buff[3]));
+	tmp = fread(buff, sizeofchar, 4, iimage); // read total instruction number
+	data_size = (((unsigned char)buff[0])<<24)|(((unsigned char)buff[1])<<16)|(((unsigned char)buff[2])<<8)|(((unsigned char)buff[3]));
+	for (int i = PC/4; tmp = fread(buff, sizeofchar, 4, iimage); i++) {
+		memi[i] = (((unsigned char)buff[0])<<24)|(((unsigned char)buff[1])<<16)|(((unsigned char)buff[2])<<8)|(((unsigned char)buff[3]));
+	}
+#ifdef log
+	printf("done\n");
+#endif
+	
+#ifdef log
+	printf(">>> read_data() done\n");
+#endif
 }
